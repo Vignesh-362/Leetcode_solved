4fa05eddeb03b5cba73ef1585c73ef31119c7280
Vignesh-362 <vv299213@gmail.com>
2026-02-05 21:33:26 +0530

    5/2/26

---
+++ Medium/sumFourDivisors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int n=nums.size(),total=0,c,sum;
+        for(int i=0;i<n;i++)
+        {
+            c=2;
+            sum= nums[i] +1;
+            for(int j=2;j<=(nums[i]/2);j++)
+             {
+                if(c==5)  break;
+                if(nums[i] % j ==git 0) 
+                {
+                    sum+= j;
+                    c++;
+                }
+             }
+            if(c==4) total+=sum;
+         }
+        return total;
+    }
+};
