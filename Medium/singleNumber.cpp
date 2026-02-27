94a5713397b318fbc8a41b31a04990e7228bd597
Vignesh-362 <vv299213@gmail.com>
2026-02-27 21:52:56 +0530

    27/2/26

---
+++ Medium/singleNumber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=0 ;i< 32 ;i++)
+        {
+            int sum=0;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(1<<i & nums[j]) sum++;
+            }
+
+            if(sum % 3 ==1) ans += 1<<i;
+        }
+        return ans;
+    }
+};
